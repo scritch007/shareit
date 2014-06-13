9b536196d193c89440d62fad9f87359158b23059
Legrand Benjamin <dev@legrand.ws>
2014-06-13 14:36:50 +0200

    Improve UI and add delete popup and download popup

--- html/js/main.js
+++ html/js/main.js
@@ -69,7 +69,7 @@ function display(result){
 		img.src = "img/Folder.svg";
 		img.className = "icon";
 		var parent = document.createElement("div");
-		parent.className = "files_item list-content bg-cyan";
+		parent.className = "files_item list-content bg-lightBlue";
 		parent.appendChild(img);
 		var parentLabel = document.createElement("span");
 
@@ -100,7 +100,7 @@ function display(result){
 		var img = document.createElement("img");
 		img.className = "icon";
 		var li = document.createElement("div");
-		li.className = "files_item list-content bg-cyan";
+		li.className = "files_item list-content bg-lightBlue";
 		li.appendChild(img);
 		var elementDiv = document.createElement("div");
 		elementDiv.className = "data";
@@ -108,27 +108,17 @@ function display(result){
 		elementNameLabel.className = "list-title";
 		elementNameLabel.innerHTML = element.name;
 		elementDiv.appendChild(elementNameLabel);
+		var buttonDiv = document.createElement("div");
+
 		var deleteButton = document.createElement("input");
 		deleteButton.type = "button";
 		deleteButton.value = "delete";
 		deleteButton.onclick = function(path, event){
-			event.stopPropagation()
-			command = {
-				name: "browser.delete_item",
-				delete_command:{
-					"path": path
-				}
-			}
-			$.post(
-				"/commands",
-				JSON.stringify(command),
-				function(result){
-					browse(current_folder);
-				},
-				"json"
-			);
+			event.stopPropagation();
+			document.body.appendChild(deletePopup(path));
 		}.bind(element, element_path);
-		elementDiv.appendChild(deleteButton);
+		buttonDiv.appendChild(deleteButton);
+		elementDiv.appendChild(buttonDiv);
 
 		if (element.isDir){
 			li.onclick = function(path){
@@ -140,6 +130,7 @@ function display(result){
 			var downloadButton = document.createElement("input");
 			downloadButton.type = "button";
 			downloadButton.value = "download";
+			downloadButton.className = "button primary";
 			downloadButton.onclick = function(path, event){
 				event.stopPropagation()
 				command = {
@@ -152,12 +143,14 @@ function display(result){
 					"/commands",
 					JSON.stringify(command),
 					function(result){
-						console.log(result.download_link.result);
+						console.log(result.download_link_command.download_link);
+						document.body.appendChild(downloadPopup(path, result.download_link_command.download_link));
 					},
 					"json"
 				);
 			}.bind(element, element_path);
-			elementDiv.appendChild(downloadButton);
+			buttonDiv.appendChild(document.createTextNode('\u00A0'));
+			buttonDiv.appendChild(downloadButton);
 		}
 		li.appendChild(elementDiv);
 		a.appendChild(li);
@@ -218,6 +211,125 @@ function createFolder(){
 	createFolderPopup.appendChild(buttonDiv);
 	document.body.appendChild(createFolderPopup);
 	folderNameInput.focus();
+}
+
+function downloadPopup(path, download_link){
+	var window_div = document.createElement("div");
+	window_div.className = "window shadow";
+	window_div.id = "download_link_popup";
+	var caption_div = document.createElement("div");
+	caption_div.className = "caption";
+	//Caption definition
+	var caption_span = document.createElement("span");
+	caption_span.className = "icon icon-windows";
+	caption_div.appendChild(caption_span);
+	var caption_title = document.createElement("div");
+	caption_title.className = "title";
+	caption_title.innerHTML = "Download " + path;
+	caption_div.appendChild(caption_title);
+	var caption_close_button = document.createElement("button");
+	caption_close_button.type = "button";
+	caption_close_button.className = "btn-close";
+	caption_close_button.onclick = function(){
+		window_div.parentNode.removeChild(window_div);
+	}
+	caption_div.appendChild(caption_close_button);
+	//End of Caption defintion
+	window_div.appendChild(caption_div);
+	var content_div = document.createElement("div");
+	content_div.className = "content";
+	content_div.id = "download_link_content";
+	var display_content_div = document.createElement("div");
+	var url_div = document.createElement("div");
+	url_div.className = "input-control text";
+	var download_link_input = document.createElement("input");
+	download_link_input.type = "text";
+	var dlink = location.protocol + "//" + location.host + "/downloads/" + download_link;
+	download_link_input.value = dlink;
+	url_div.appendChild(download_link_input);
+	url_div.appendChild(document.createTextNode('\u00A0'));
+	var download_link_download_button = document.createElement("span");
+	//download_link_download_button.type = "button";
+	download_link_download_button.className = "icon icon-download";
+	download_link_download_button.onclick = function(){
+		window.open(dlink);
+	}
+	url_div.appendChild(download_link_download_button);
+	display_content_div.appendChild(url_div);
+	content_div.appendChild(display_content_div);
+	window_div.appendChild(content_div);
+	return window_div;
+}
+
+function deletePopup(path){
+	var window_div = document.createElement("div");
+	window_div.className = "window shadow";
+	window_div.id = "delete_item_popup";
+	var caption_div = document.createElement("div");
+	caption_div.className = "caption";
+	//Caption definition
+	var caption_span = document.createElement("span");
+	caption_span.className = "icon icon-windows";
+	caption_div.appendChild(caption_span);
+	var caption_title = document.createElement("div");
+	caption_title.className = "title";
+	caption_title.innerHTML = "Delete " + path;
+	caption_div.appendChild(caption_title);
+	var caption_close_button = document.createElement("button");
+	caption_close_button.type = "button";
+	caption_close_button.className = "btn-close";
+	caption_div.appendChild(caption_close_button);
+	caption_close_button.onclick = function(){
+		window_div.parentNode.removeChild(window_div);
+	}
+	//End of Caption defintion
+	window_div.appendChild(caption_div);
+	var content_div = document.createElement("div");
+	content_div.className = "content";
+	content_div.id = "delete_item_content";
+	var h3 = document.createElement("h3");
+	h3.innerHTML = "Do you want to remove " + path;
+
+	content_div.appendChild(h3);
+	var buttonDiv = document.createElement("div");
+	buttonDiv.className = "form-actions"
+
+	var ok_button = document.createElement("input");
+	ok_button.type = "button";
+	ok_button.value = "Yes";
+	ok_button.className = "button primary";
+	ok_button.onclick = function(){
+		command = {
+			name: "browser.delete_item",
+			delete_command:{
+				"path": path
+			}
+		}
+		$.post(
+			"/commands",
+			JSON.stringify(command),
+			function(result){
+				window_div.parentNode.removeChild(window_div);
+				browse(current_folder);
+			},
+			"json"
+		);
+	};
+	buttonDiv.appendChild(ok_button);
+	var spacer = document.createTextNode('\u00A0');
+	spacer.className="spacer";
+	buttonDiv.appendChild(spacer);
+	var cancel_button = document.createElement("input");
+	cancel_button.type = "button";
+	cancel_button.value = "No";
+	cancel_button.className = "button";
+	cancel_button.onclick = function(){
+		window_div.parentNode.removeChild(window_div);
+	}
+	buttonDiv.appendChild(cancel_button);
+	content_div.appendChild(buttonDiv);
+	window_div.appendChild(content_div);
+	return window_div;
 }
 /*
 $.ajax({
